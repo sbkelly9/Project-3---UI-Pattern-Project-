e97c8acda14a137a4de61f4cb0cf19072ccd2a00
Stu Kelly <sbkelly9@gmail.com>
2020-02-11 09:27:21 -0500

    trying to fix CORS issue

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-let url = 'https://swapi.co/api/'
+const baseUrl = 'https://swapi.co/api/people'
 
-fetch("https://swapi.co/api/people/1/")
-  .then(res => {
-    console.log("success!", res);
-  })
-  .catch(err => {
-    console.log("something went wrong...", err);
-  });
+function fetchCharacter(event) {
+	const id = event.target.getAttribute("data-charid")
+	const url = `${baseUrl}/${id}`
+
+	fetch(url)
+		.then(res => {
+			let response = res.json();
+			response.then(characterInfo => {
+				event.preventDefault();
+				this.blur(); // Manually remove focus from clicked link.
+
+				const html = $('#ex1')
+				const elementIdArray = ["name", "height", "mass", "hair_color", "skin_color", "eye_color", "birth_year", "gender"]
+
+				elementIdArray.forEach(elemId => {
+					$(`#${elemId}`).text(`${elemId}: ${characterInfo[elemId]}`)
+				})
+
+				$(html).appendTo('body').modal()
+			});
+		})
+		.catch(err => {
+			console.log("something went wrong...", err);
+		});
+}
+
+document.querySelectorAll(".container").forEach(element => {
+	$(element).click(fetchCharacter)
+})
+
+
